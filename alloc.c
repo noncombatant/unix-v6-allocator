5384b9d90d6abf02566b6934f3c48bf34d105ba3
chris <chris@noncombatant.org>
2021-11-28 14:28:13 -0800

    Format

--- alloc.c
+++ alloc.c
@@ -7,70 +7,69 @@
 
 #include "alloc.h"
 
-#define	logical	char *
+#define logical char*
 
 struct fb {
-	size_t	size;
-	struct fb* next;
+  size_t size;
+  struct fb* next;
 };
 
-struct fb	freelist[] = {
-  { 0, -1, }
-};
-size_t	slop = sizeof(void*);
+struct fb freelist[] = {{
+    0,
+    -1,
+}};
+size_t slop = sizeof(void*);
 
-void* v6alloc(size_t asize)
-{
-	register size_t size;
-	register struct fb* np;
-	register struct fb* cp;
+void* v6alloc(size_t asize) {
+  register size_t size;
+  register struct fb* np;
+  register struct fb* cp;
 
-	if ((size = asize) == 0)
-		return(0);
-	size += 3 * sizeof(void*);
-	size &= ~01;
-	for (;;) {
-		for (cp=freelist; (np= cp->next) != -1; cp=np) {
-			if (np->size>=size) {
-				if (size+slop >= np->size) {
-					cp->next = np->next;
-					return(&np->next);
-				}
-				cp = cp->next = np+size;
-				cp->size = np->size - size;
-				cp->next = np->next;
-				np->size = size;
-				return(&np->next);
-			}
-		}
-		asize = size<1024? 1024: size;
-		if ((cp = sbrk(asize)) == -1) {
-			return (-1);
-		}
-		cp->size = asize;
-		v6free(&cp->next);
-	}
+  if ((size = asize) == 0)
+    return (0);
+  size += 3 * sizeof(void*);
+  size &= ~01;
+  for (;;) {
+    for (cp = freelist; (np = cp->next) != -1; cp = np) {
+      if (np->size >= size) {
+        if (size + slop >= np->size) {
+          cp->next = np->next;
+          return (&np->next);
+        }
+        cp = cp->next = np + size;
+        cp->size = np->size - size;
+        cp->next = np->next;
+        np->size = size;
+        return (&np->next);
+      }
+    }
+    asize = size < 1024 ? 1024 : size;
+    if ((cp = sbrk(asize)) == -1) {
+      return (-1);
+    }
+    cp->size = asize;
+    v6free(&cp->next);
+  }
 }
 
-void v6free(char* aptr)
-{
-	register struct fb* ptr;
-	register struct fb* cp;
-	register struct fb* np;
+void v6free(char* aptr) {
+  register struct fb* ptr;
+  register struct fb* cp;
+  register struct fb* np;
 
-	ptr = aptr-sizeof(void*);
-	cp = freelist;
-	while ((np = cp->next) < ptr)
-		cp = np;
-	if (ptr+ptr->size == np) {
-		ptr->size =+ np->size;
-		ptr->next = np->next;
-		np = ptr;
-	} else
-		ptr->next = np;
-	if (cp+cp->size == ptr) {
-		cp->size =+ ptr->size;
-		cp->next = ptr->next;
-	} else
-		cp->next = ptr;
+  ptr = aptr - sizeof(void*);
+  cp = freelist;
+  while ((np = cp->next) < ptr)
+    cp = np;
+  if (ptr + ptr->size == np) {
+    ptr->size = +np->size;
+    ptr->next = np->next;
+    np = ptr;
+  } else
+    ptr->next = np;
+  if (cp + cp->size == ptr) {
+    cp->size = +ptr->size;
+    cp->next = ptr->next;
+  } else
+    cp->next = ptr;
 }
--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include "alloc.h"
 
 int main() {
-  size_t sizes[] = { 100, 1000, 333 };
+  size_t sizes[] = {100, 1000, 333};
   for (int i = 0; i < 3; i++) {
     size_t s = sizes[i];
     char* p = v6alloc(s);
