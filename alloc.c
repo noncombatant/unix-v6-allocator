d9405d980e8f3def3f2717d9f2b91f67c5095818
chris <chris@noncombatant.org>
2021-11-28 14:54:16 -0800

    Modern declaration and block style
    Move declarations closer to their first use, including reducing scope where
    possible. Initialize where possible.
    
    Use curly braces even for 1-line blocks, for clarity.
    
    Don't use parentheses around the `return` value.

--- alloc.c
+++ alloc.c
@@ -19,31 +19,32 @@ FreelistBlock freelist[] = {{
 size_t slop = sizeof(void*);
 
 void* v6alloc(size_t asize) {
-  size_t size;
-  FreelistBlock* next;
-  FreelistBlock* current;
-
-  if ((size = asize) == 0)
-    return (0);
+  if (asize == 0) {
+    return 0;
+  }
+  size_t size = asize;
   size += 3 * sizeof(void*);
   size &= ~01;
+
   for (;;) {
+    FreelistBlock* next;
+    FreelistBlock* current;
     for (current = freelist; (next = current->next) != -1; current = next) {
       if (next->size >= size) {
         if (size + slop >= next->size) {
           current->next = next->next;
-          return (&next->next);
+          return &next->next;
         }
         current = current->next = next + size;
         current->size = next->size - size;
         current->next = next->next;
         next->size = size;
-        return (&next->next);
+        return &next->next;
       }
     }
     asize = size < 1024 ? 1024 : size;
     if ((current = sbrk(asize)) == -1) {
-      return (-1);
+      return -1;
     }
     current->size = asize;
     v6free(&current->next);
@@ -51,23 +52,23 @@ void* v6alloc(size_t asize) {
 }
 
 void v6free(char* aptr) {
-  FreelistBlock* ptr;
-  FreelistBlock* current;
+  FreelistBlock* ptr = aptr - sizeof(void*);
+  FreelistBlock* current = freelist;
   FreelistBlock* next;
-
-  ptr = aptr - sizeof(void*);
-  current = freelist;
-  while ((next = current->next) < ptr)
+  while ((next = current->next) < ptr) {
     current = next;
+  }
   if (ptr + ptr->size == next) {
     ptr->size = +next->size;
     ptr->next = next->next;
     next = ptr;
-  } else
+  } else {
     ptr->next = next;
+  }
   if (current + current->size == ptr) {
     current->size = +ptr->size;
     current->next = ptr->next;
-  } else
+  } else {
     current->next = ptr;
+  }
 }
